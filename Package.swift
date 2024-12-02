4b25de061252a18a8cb62adb80ebad4e47191815
pm-sneha-firodiya <47361492+pm-sneha-firodiya@users.noreply.github.com>
2024-12-02 13:33:37 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,20 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "OpenWrapHandlerDFP-Swift-Package",
+    name: "OpenWrapHandlerDFP",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "OpenWrapHandlerDFP-Swift-Package",
-            targets: ["OpenWrapHandlerDFP-Swift-Package"]),
+            name: "OpenWrapHandlerDFP",
+            targets: ["OpenWrapHandlerDFP"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "OpenWrapHandlerDFP-Swift-Package"),
-        .testTarget(
-            name: "OpenWrapHandlerDFP-Swift-PackageTests",
-            dependencies: ["OpenWrapHandlerDFP-Swift-Package"]),
+        .binaryTarget(name: "OpenWrapHandlerDFP", url: "https://github.com/pm-sneha-firodiya/OpenWrapHandlerDFP-Swift-Package/releases/download/1.0.0/OpenWrapHandlerDFP.xcframework.zip", checksum: "6bc9e060807a3db9d5ed9f112e62299f1b882445568fd1428dfa6a949bc35e23")
     ]
 )
